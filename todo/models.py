dae0af1851214d49dd9be3bb91a0267f9c92c2ed
unknown <mmohamedabdelm@gmail.com>
2024-01-24 17:53:28 +0200

    add models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+
+STATUS_TASK = {
+    'todo': 'todo',
+    'inprogress': 'inprogress',
+    'finished': 'finished',
+}
+
+class Tasks(models.Model):
+    user = models.ForeignKey(User, related_name = 'user_task', on_delete= models.CASCADE)
+    task = models.TextField(max_length=200)
+    status = models.CharField(max_length=20, choices=STATUS_TASK.items())
+    created_at = models.DateTimeField(timezone.now)
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.task
+    
+
+
+
